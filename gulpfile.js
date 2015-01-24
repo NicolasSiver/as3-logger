2e73e9d7aff28545a869952dc6117c4d652fbfd8
Nicolas Siver <nicolas@siver.im>
2015-01-24 15:32:29 +0200

    Compilation of swc with defines

--- gulpfile.js
+++ gulpfile.js
@@ -6,20 +6,47 @@ var gulp = require('gulp'),
     Q = require('q');
 
 var playerVersion = '16.0';
-// var flexSdk = path.jnhome', 'Documents', 'ActionScript', 'sdk', 'apache-flex-4.13.0');
 var flexSdk = '/home/nicolas/Documents/ActionScript/sdk/apache-flex-4.13.0';
 var airSdk = path.join('D:', 'Works', 'ActionScript', 'SDK', 'AIR-16.0.0');
 var compc = path.resolve(flexSdk, './bin/compc');
 var libs = path.resolve(flexSdk, './frameworks/libs');
-var playerGlobal = path.resolve(libs, util.format('./player/%s/playerglobal.swc', playerVersion));
 
-// process.env['PLAYERGLOBAL_HOME'] = path.resolve(libs, 'player');
 
-gulp.task('create:swc', function() {
+gulp.task('create:swc-web', function() {
     var deferred = Q.defer();
-    var execCommand = createCompc(compc, 'lib', {'MOBILE':true, 'DEFAULT':false});
-    gutil.log('exec: ' + execCommand);
-    exec(createCompc(compc, 'lib'), function(error, stdout, stderr) {
+    execCommand(createCompc(compc, 'logger', 'web'), deferred);
+    return deferred.promise;
+});
+
+gulp.task('create:swc-mobile', function() {
+    var deferred = Q.defer();
+    execCommand(createCompc(compc, 'logger-mobile', 'mobile'), deferred);
+    return deferred.promise;
+});
+
+gulp.task('default', ['create:swc-web', 'create:swc-mobile']);
+
+function addCommand(app, command) {
+    app += ' ' + command;
+    return app;
+}
+
+function createCompc(app, libName, libType) {
+    var result = app;
+
+    result = addCommand(result, '-compiler.debug=false');
+    result = addCommand(result, util.format('-output=application/export/%s.swc', libName));
+    result = addCommand(result, '-include-sources=client/src');
+    result = addCommand(result, '-source-path=client/src');
+    result = addCommand(result, util.format('-target-player=%s', playerVersion));
+    result = addCommand(result, util.format('-load-config=%s', 'lib-config.xml'));
+    result = addCommand(result, util.format('-load-config+=def-%s.xml', libType));
+    return result;
+}
+
+function execCommand(command, deferred) {
+    gutil.log('exec: ' + command);
+    exec(command, function(error, stdout, stderr) {
         gutil.log(gutil.colors.yellow(stdout));
         if(stderr){
             gutil.log(gutil.colors.red('Error: ' + stderr));
@@ -29,24 +56,4 @@ gulp.task('create:swc', function() {
         // }
         deferred.resolve();
     });
-    return deferred.promise;
-});
-
-gulp.task('default', ['create:swc']);
-
-function addCommand(app, command) {
-    app += ' ' + command;
-    return app;
-}
-
-function createCompc(app, libName) {
-    var result = app;
-
-    result = addCommand(result, '-compiler.debug=false');
-    result = addCommand(result, util.format('-output=application/export/%s.swc', libName));
-    result = addCommand(result, '-include-sources=client/src');
-    result = addCommand(result, '-source-path=client/src');
-    result = addCommand(result, util.format('-target-player=%s', playerVersion));
-    result = addCommand(result, util.format('-load-config=%s', 'lib-config.xml'));
-    return result;
 }
